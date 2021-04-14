b68c45c60c8ecd3fd838bccc3ceee48015908d64
Herpoel Quentin <herpoel.quentin@gmail.com>
2021-04-14 10:49:01 +0200

    fondu ok & scrolls adaptés

--- js/controller.js
+++ js/controller.js
@@ -5,15 +5,23 @@ const template = new Template;
 export class Controller {
   body = document.getElementById('body');
 
+  getHomePage = () => {
+    this.getAnimation(template.getHomePageTemplate());
+  };
+
   getCv = () => {
+    this.getAnimation(template.getCvTemplate());
+  };
+
+  getAnimation = (template) => {
     body.style.opacity = 0;
     setTimeout(() => {
       body.style.transition = 'opacity .3s cubic-bezier(0.075, 0.82, 0.165, 1)';
-      
+
     }, 1);
-    body.innerHTML = template.getCvTemplate();
+    body.innerHTML = template;
     setTimeout(() => {
-      body.style.opacity = 100;
+      body.style.opacity = 500;
     }, 1);
   }
 };
--- js/templates.js
+++ js/templates.js
@@ -1,10 +1,43 @@
 export class Template {
 
+  getHomePageTemplate = () => {
+    return `
+    <div class="pic">
+    <img src="/src/portrait.jpg" alt="my face">
+    </div>
+  <div class="presentation">
+    <div class="names">
+      <p>Hello !</p>
+      <p>I'm Quentin, I am an IT student.</p>
+      <p>I love coding, learning and playing with my children.</p>
+    </div>
+    <ul class="social-container">
+      <li><a href="https://twitter.com/90toofi"><img class="social-link" src="/src/twitter-5-512.png"
+            alt="twitter link"></a></li>
+      <li><a href="https://www.facebook.com/quentin.h.toofi"><img class="social-link" src="/src/facebook-5-512.png"
+            alt="facebook link"></a></li>
+      <li><a href="https://https://github.com/Toofi"><img class="social-link" src="/src/github-10-512.png"
+            alt="github link"></a></li>
+      <li><a href="https://www.linkedin.com/in/herpoel-quentin-834147146/"><img class="social-link"
+            src="/src/linkedin-5-512.png" alt="linkedin link"></a></li>
+    </ul>
+    <ul>
+      <p>My works :</p>
+      <li><a class="link" href="https://github.com/Toofi/tf-framework">A never finished CSS Framework</a></li>
+      <li><a class="link" href="https://github.com/Toofi/url-reducer">An url reducer</a></li>
+      <li><a class="link" href="https://github.com/Toofi/wp-theme">My own Wordpress theme</a></li>
+      <li><a class="link" href="https://github.com/Toofi/restaurantApp">An Angular app I made for my first
+          internship</a></li>
+    </ul>
+  </div>
+    `
+  };
+
   getCvTemplate = () => {
     return `
     <div class="pic cv">
     <img src="/src/portrait.jpg" alt="my face">
-  </div>
+    </div>
     <div class="cv-container">
       <ul class="cv-ul">
         <p class="cv-title experience">IT :</p>
